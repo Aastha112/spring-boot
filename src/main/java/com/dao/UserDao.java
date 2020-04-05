c3e4781ff145d20009448eacbb85970d2dc0b5ca
Aastha112 <63099576+Aastha112@users.noreply.github.com>
2020-04-05 14:50:26 +0530

    first commit

---
+++ src/main/java/com/dao/UserDao.java
@@ -0,0 +1,47 @@
+package com.dao;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Repository;
+
+import com.bean.UserBean;
+
+@Repository
+public class UserDao {
+	// contains all user data
+	ArrayList<UserBean> users = new ArrayList<>();
+
+	// will add user in array list
+	public boolean insertUser(UserBean userBean) {
+		boolean isError = users.add(userBean);
+		return isError;
+	}
+
+	// will return entire list of user
+	public ArrayList<UserBean> listUsers() {
+		return users;
+	}
+
+	public UserBean getUserByEmail(String email) {
+		// search from entier list one by one
+		for (UserBean u : users) {
+			// match given email from user object
+			if (u.getEmail().equals(email)) {
+				return u;// if match then return user
+			}
+		}
+
+		return null; // if not match then last we send null
+	}
+
+	public void deleteUser(String email) {
+		for (UserBean u : users) {
+			if (u.getEmail().equals(email)) {
+				users.remove(u);
+				break;
+			}
+
+		}
+
+	}
+}
